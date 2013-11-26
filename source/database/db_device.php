13414ac37044f37686a5113b9314114d942c77f8
Tommy Knudsen <zfire911@gmail.com>
2013-11-26 11:12:31 +0100

    scope is still hard

--- source/database/db_device.php
+++ source/database/db_device.php
@@ -1,7 +1,8 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'].'/spc/source/database/sqlHelper.php');
-$db = new MySQLHelper();
+
 function db_device_turn_on($cId,$tId) {
+	$db = new MySQLHelper();
 	$db->autocommit(false);
 	$db->executeSQL("INSERT INTO controller_used_by_tag VALUES ('$tId','$cId',NOW(),)"); //time=time when device was turned on, user=the user who turned the device on
 	$db->executeSQL("UPDATE controller SET status='GREEN' WHERE CSerieNo='$cId'");
@@ -10,6 +11,7 @@ function db_device_turn_on($cId,$tId) {
 }
 
 function db_device_turn_off($cId,$tId) {
+	$db = new MySQLHelper();
 	$db->autocommit(false);
 	$time = $db->executeSQL("SELECT UNIX_TIMESTAMP(starttime) FROM controller_used_by_tag WHERE CSerieNo='$cId' AND TSerieNo='$tId' AND endtime IS NULL")->fetch_assoc()['starttime'];
 	$timeSpent = floor((time()-$time)/60);
@@ -24,6 +26,7 @@ function db_device_turn_off($cId,$tId) {
 //Returns true/false
 function db_device_verify_cId($cId,$tId)
 {
+	$db = new MySQLHelper();
 	$result = $db->executeSQL("SELECT TRUE FROM control_system,tag,controller WHERE controller.CSerieNo='$cId' AND tag.TSerieNo='$tId' AND tag.CSId=control_system.CSId AND controller.CSId=control_system.CSId");
 	if ($result->num_rows > 0)
 	{
