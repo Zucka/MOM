6779c4328d0656dd90342e61c6331ed09cf050a3
Tommy Knudsen <zfire911@gmail.com>
2013-11-26 11:01:20 +0100

    another small fix

--- source/api/api.php
+++ source/api/api.php
@@ -1,7 +1,7 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'].'/database/sqlHelper.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/database/db_device.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/database/db_points.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/spc/database/sqlHelper.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/spc/database/db_device.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/spc/database/db_points.php');
 require 'vendor/autoload.php';
 $db = new MySQLHelper();
 $app = new \Slim\Slim();
