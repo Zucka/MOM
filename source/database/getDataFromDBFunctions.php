d8af186bc04c208bfe51a307ec9642ea531649a9
Lisbeth Nielsen <lniel1989@hotmail.com>
2013-11-27 10:30:14 +0100

    comments is added

--- source/database/getDataFromDBFunctions.php
+++ source/database/getDataFromDBFunctions.php
@@ -1,4 +1,5 @@
 <?php
+	/*get all profiles from a system id*/
 	function profilesByCSId($CSId)
 	{
 		$db= new MySQLHelper();
@@ -15,7 +16,7 @@
 		}
 		return $returnArray;
 	}
-	
+	/* get all controller from a system id */
 	function controllersByCSId($CSId)
 	{
 		$db= new MySQLHelper();
@@ -38,7 +39,7 @@
 		}
 		return $returnArray;
 	}
-
+    /* get all tags from a system id */
 	function tagsByCSId($CSId)
 	{
 		$db= new MySQLHelper();
@@ -61,7 +62,7 @@
 		}
 		return $returnArray;
 	}
-
+	/* get the x latest users who have used the controller. This is used by controllersByCSId*/
 	function controllersLastUsedHelpFunc($controllerID, $limitFrom=0, $limitTo=1)
 	{
 		$db= new MySQLHelper();
@@ -83,7 +84,7 @@
 		$result = $db->query( $selectValues , $table, $whereClause, $ordering , $otherSQL);
 		return  mysqli_fetch_assoc($result);
 	}
-	
+	/* get the x latest controller which this tagId have activated. This is used by tagsByCSId */
 	function tagLastUsed($tagID, $limitFrom = 0, $limitTo = 1)
 	{
 		$db= new MySQLHelper();
@@ -104,5 +105,40 @@
 		return mysqli_fetch_assoc($result);
 	}
 	
-
+	/* get rules and permission from a personID*/
+	/*this is in progress
+	function getRulesFromPId($personId, $isPermission = false)
+	{//($selectValues, $tables, $whereClause = NULL, $ordering = NULL, $otherSQL = NULL, $distinctResults = false)
+		$db= new MySQLHelper();
+		global $theTables;
+		global $theColumns;
+		$columnCond =  $theColumns['Rcondition'];
+		$columnCondTP = $theColumns['Cond_timeperiod'];
+		$columnCondTS = $theColumns['Cond_timestamp'];
+		$columnAction = $theColumns['Action'];
+		$columnRules = $theColumns['Rules'];
+		$columnPHR = $theColumns['Profile_has_rules'];
+		
+		
+		$selectValues='*';
+		$tables = $theTables['Profile_has_rules']. ',' . $theTables['Rules'];
+		$whereClause = $columnPHR[0] . ' = '. $personId . ' AND ' . $columnPHR[1] . '=' $columnRules[0]. 
+		if($isPermission)
+		{
+			$tables .= ',' . $theTables['Rcondition'] . ',' . $theTables['Cond_timeperiod'];
+			$whereClause .= ' AND '. $columnRules[3] .'='. $isPermission . ' AND ' . $columnCond[1] . "=". $columnRules[0].  ' AND ' . $columnCondTP[1] . "=". $columnCond[0]; 
+		
+		}
+		else
+		{
+		
+		}
+		$result = $db->query($selectValues, $table, $whereClause );
+		$returnArray = null;
+		while($row = mysqli_fetch_assoc($result))
+		{	
+			$returnArray[] = $row; 
+		}
+		return $returnArray;
+	}*/
 ?>
