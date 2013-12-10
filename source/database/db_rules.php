63804bf22cefeb21ec5c1c3760d827e74166f1a2
Lisbeth Nielsen <lniel1989@hotmail.com>
2013-12-10 14:59:56 +0100

    update noget af det virker

--- source/database/db_rules.php
+++ source/database/db_rules.php
@@ -1,11 +1,18 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'].'/spc/source/database/sqlHelper.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/spc/source/database/getDataFromDBFunctions.php');
+include_once "getDataFromDBFunctions.php";
 
 function db_rules_user_can_turn_device_on($cId,$tId)
 {
 	$db = new MySQLHelper();
 	$pId = $db->executeSQL("SELECT profileId from tag where tag.TSerieNo='$tId'")->fetch_assoc()['profileId'];
+	if(isTagActive($tId) == false || isProfileActive($pId) == false)
+	{
+		return true
+	}
+	
+	$rules = getRulesFromPId($pId,true);
 	$permission = getRulesFromPId($pId,true);
 	//return $permission;
 	return true; //temporary
@@ -19,7 +26,24 @@ function db_rules_device_should_turn_off($cId)
 
 function db_rules_user_has_unlimited_points($pId)
 {
-	return false;
+	return hasPersonIdUnlimitedPoints($pId);
+}
+
+function get_relevant_rules($rules)
+{
+	if($rules != null)
+	{	foreach($rules as $index => $rule)
+		{
+			foreach($rule['actions'] as $action)
+			{
+				if($action['name'] != 'Access any controller' || $action['name'] != 'Cannot access any controller'
+					|| $action['name'] != 'Access controller' || $action['name'] != 'Cannot access controller')
+				{
+					unset($arrayOfCondition[$index]);
+				}
+			}
+		}
+	}	
 }
 
 ?>
