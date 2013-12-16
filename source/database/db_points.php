abf7e90fe4b6c5ebd1928f088dfa9d78753dee48
Lars Chr. Pedersen <fruitdrops13@hotmail.com>
2013-12-16 13:22:24 +0100

    Profile add/remove points test

--- source/database/db_points.php
+++ source/database/db_points.php
@@ -1,16 +1,25 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'].'/spc/source/database/sqlHelper.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/spc/source/database/DBfunctions.php');
-function db_points_remove($tId,$points)
+function db_points_remove($id,$points,$isPId = false)
 {
+	
 	$db = new MySQLHelper();
 	$db->autocommit(FALSE);
-	$row = $db->executeSQL("SELECT points,PId FROM profile,tag WHERE tag.TSerieNo='$tId' AND tag.profileId=profile.PId")->fetch_assoc();
-	$pId = $row['PId'];
-	$currentPoints = $row['points']-$points;
+	if(!$isPId){
+		$row = $db->executeSQL("SELECT points,PId FROM profile,tag WHERE tag.TSerieNo='$id' AND tag.profileId=profile.PId")->fetch_assoc();
+		$pId = $row['PId'];
+		$currentPoints = $row['points']-$points;
+	}
+	else{
+		$row = $db->executeSQL("SELECT points FROM profile WHERE PId = '$id'")->fetch_assoc();
+		$pId = $id;
+		$currentPoints = $row['points']-$points;
+	}
 	if ($currentPoints < 0) 
 	{
 		//something wrong, this should not happen!
+		$result = false;
 	}
 	else
 	{
@@ -21,13 +30,18 @@ function db_points_remove($tId,$points)
 	return $result;
 }
 
-function db_points_add($tId,$points)
+function db_points_add($id,$points,$isPid = false)
 {
 	$db = new MySQLHelper();
 	$db->autocommit(FALSE);
-	$row = $db->executeSQL("SELECT points,PId FROM profile,tag WHERE tag.TSerieNo='$tId' AND tag.profileId=profile.PId")->fetch_assoc();
-	//$currentPoints = $row['points']+$points;
-	$pId = $row['PId'];
+	if(!$isPid){
+		$row = $db->executeSQL("SELECT points,PId FROM profile,tag WHERE tag.TSerieNo='$id' AND tag.profileId=profile.PId")->fetch_assoc();
+		//$currentPoints = $row['points']+$points;
+		$pId = $row['PId'];
+	}
+	else{
+		$pId = $id;
+	}
 	$result = addPointsToProfile($pId, $points);//From lisbeth use addPointsToProfile($profileId, $points) in DBfunction it takes max point into account
 	//$db->executeSQL("UPDATE profile SET points='$currentPoints' WHERE PId='$pId'"); //need to change to take point ceiling into account
 	
