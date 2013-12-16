ba79022a823c191c95e393384cde322cf237cb8a
Lars Chr. Pedersen <fruitdrops13@hotmail.com>
2013-12-16 16:05:32 +0100

    User Edit - Password and Username checks

--- source/database/errorMessageSQL.php
+++ source/database/errorMessageSQL.php
@@ -1,10 +1,10 @@
 <?php
 
-	$SQL_ERROR_VALUE_ERROR="The value is invalid"; 
-	$SQL_ERROR_OTHER="some sql error";
-	$SQL_ERROR_NOTNULL="value must not be null";
-	$SQL_ERROR_BAD_INPUT= "bad input: column not found";
-	$SQL_ERROR_DELETE_FAILED = "delete failed due to key constraint";
-	$SQL_ERROR_WEIRD_FALSE = "something weird happen when tag->active=false use 0"
+	$SQL_ERROR_VALUE_ERROR = "The value is invalid or duplicated"; 
+	$SQL_ERROR_OTHER = "Some other SQL error";
+	$SQL_ERROR_NOTNULL = "Value must not be null";
+	$SQL_ERROR_BAD_INPUT = "Bad input: Column not found";
+	$SQL_ERROR_DELETE_FAILED = "Cannot delete or update a parent row: a foreign key constraint fails";
+	$SQL_ERROR_WEIRD_FALSE = "You have an error in your SQL syntax."//"something weird happen when tag->active=false use 0"
 
 ?>
